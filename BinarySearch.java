f3d1765b8345aa4cd0b3cbfc10c7368ae3466f74
Israel <idh@case.edu>
2015-08-23 14:50:44 -0400

    added iterative binary search method

--- BinarySearch.java
+++ BinarySearch.java
@@ -24,10 +24,31 @@ public class BinarySearch {
     }
   }
 
+  public int iterativeBinarySearch(int[] array, int key, int imin, int imax) {
+    while(imin <= imax) {
+      int imid = (imax + imin) / 2;
+      if(array[imid] == key) {
+        //found key
+        return imid;
+      }
+      else if(key < array[imid]) {
+        //key in lower half of array -> update indecie of imax
+        imax = imid - 1;
+      }
+      else if(key > array[imid]) {
+        //key is in upper half of array -> update indecie of imin
+        imin = imid + 1;
+      }
+    }
+
+    //key was not found
+    return -1;
+  }
+
   public static void main(String[] args) {
       BinarySearch bs = new BinarySearch();
       int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 23, 34, 37, 46, 58, 69, 70, 71, 77, 79, 81, 83, 84};
       int key = 34;
-      System.out.println("Key is at index: " + bs.binarySearch(array, key, 0, array.length - 1));
+      System.out.println("Key is at index: " + bs.iterativeBinarySearch(array, key, 0, array.length - 1));
   }
 }
